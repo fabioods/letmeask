107d8eb51b9287e57bdeab1e4fc820208e4c4f7c
Fábio dos Santos <fah_ds@live.com>
2021-07-23 17:14:45 -0300

    feat: tela home estilizada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,37 @@
 import React from 'react';
-import ilustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
-
-// import { Container } from './styles';
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
 
 const Home: React.FC = () => {
-  return <div>
+  return (
+    <div id="page-auth">
       <aside>
-        <img src={ilustrationImg} alt="Ilustração simbolizando perguntas e respostas"  />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-          <div>
-            <img src={logoImg} alt="Logotipo" />
-            <button>
-              <img src={googleIconImg} alt="Logo do Google" />
-              Crie sua sala com o Google
-            </button>
-            <div> 
-              ou entre em uma sala
-            </div>
-            <form>
-              <input type="text" placeholder="Digite o código da sala" />
-              <button type="submit">Entrar na sala</button>
-            </form>
-          </div>
-         </main>
-     </div>;
-}
+        <div className="main-content">
+          <img src={logoImg} alt="Logotipo" />
+          <button type="button" className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default Home;
