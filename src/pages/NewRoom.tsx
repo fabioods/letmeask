b769feec09bb1b7c17c7ddd3a1408ec9bfa5ecdd
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-27 13:23:36 -0300

    feat: adicionada página NewRoom

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+
+const NewRoom: React.FC = () => {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logotipo" />
+          <h2>Criar uma nova Sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default NewRoom;
