991e096f2ad19cc82f7f0d1ecf78b9302c7845c0
Fábio Santos <fabio.santos@fundacaoabc.org>
2021-07-27 15:12:12 -0300

    refactor: substituida tag a pela Link do react-router-dom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import '../styles/auth.scss';
 
 const NewRoom: React.FC = () => {
   return (
@@ -25,7 +26,7 @@ const NewRoom: React.FC = () => {
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="/">Clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
