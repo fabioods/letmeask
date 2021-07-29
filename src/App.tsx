edc5111702f33d543890fc60f095fafb29847c06
Fábio dos Santos <fah_ds@live.com>
2021-07-29 20:51:36 -0300

    refactor: movida rotina de auth para o AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,18 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
 import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
-import { auth, firebase } from './services/firebase';
-
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-};
-
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-};
-
-export const AuthContext = createContext({} as AuthContextType);
 
 function App(): JSX.Element {
-  const [user, setUser] = useState<User>();
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(loggedUser => {
-      if (loggedUser) {
-        const { displayName, photoURL, uid } = loggedUser;
-        if (!displayName || !photoURL)
-          throw new Error('Missing Information from Google Account');
-        setUser({ id: uid, name: displayName, avatar: photoURL });
-      }
-    });
-    return () => unsubscribe();
-  }, []);
-
-  const signInWithGoogle = async (): Promise<void> => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    const result = await auth.signInWithPopup(provider);
-    if (result.user) {
-      const { displayName, photoURL, uid } = result.user;
-      if (!displayName || !photoURL)
-        throw new Error('Missing Information from Google Account');
-      setUser({ id: uid, name: displayName, avatar: photoURL });
-    }
-  };
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      <AuthContextProvider>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
         </Switch>
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
