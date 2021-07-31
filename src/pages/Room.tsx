dbf1608e264af878cd5314a558f389cfec3923a1
Fábio dos Santos <fah_ds@live.com>
2021-07-31 16:48:39 -0300

    feat: enviado para o RoomCode o codigo que vem de parametro na URL

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import '../styles/room.scss';
 
+type RoomParams = {
+  id: string;
+};
+
 const Room: React.FC = () => {
+  const params = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Let me ask" />
-          <RoomCode>Código</RoomCode>
+          <RoomCode code={params.id} />
         </div>
       </header>
       <main>
