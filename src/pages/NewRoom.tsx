1c518aff010e7effebbd2eea88e8d6fdbb063b80
Fábio dos Santos <fah_ds@live.com>
2021-07-31 14:47:03 -0300

    feat: criado logica para salvar sala no firebase e redirecionar para a mesma

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,12 +1,30 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 import '../styles/auth.scss';
 
 const NewRoom: React.FC = () => {
+  const { user } = useAuth();
+  const [newRoom, setNewRoom] = React.useState('');
+  const history = useHistory();
+
+  async function handleCreateRoom(e: FormEvent) {
+    e.preventDefault();
+    if (newRoom.trim() === '') return;
+    const roomRef = database.ref(`rooms}`);
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -21,8 +39,12 @@ const NewRoom: React.FC = () => {
         <div className="main-content">
           <img src={logoImg} alt="Logotipo" />
           <h2>Criar uma nova Sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={e => setNewRoom(e.target.value)}
+            />
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
