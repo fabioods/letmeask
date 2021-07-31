8d440e221e7974b83754a0879ae39ef4566cb240
Fábio dos Santos <fah_ds@live.com>
2021-07-31 14:46:27 -0300

    feat: criada rota para redirecionar para Sala

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
 import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
+import Room from './pages/Room';
 
 function App(): JSX.Element {
   return (
@@ -10,7 +11,8 @@ function App(): JSX.Element {
       <AuthContextProvider>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
